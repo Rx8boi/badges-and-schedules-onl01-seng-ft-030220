7a60b16f16e414a65c401a646bffa533701cce83
Charles Butler <cdbutle@outlook.com>
2020-03-11 19:22:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,3 +5,6 @@
 #write a "batch_badge_creator" method that takes an array of names and returns an array of badge messages
 #write a method called "assign_rooms" takes list of speakers and assigns each to a room 1 for each room
      #7 rooms  -> Return "Hello, ___! You'll be assigned to room___!"
+     # create a method called "printer" that will ouput first the results of the "batch_badge_creator" method & then the "assign_rooms" method on screen
+     
+     def badge_maker(name)
