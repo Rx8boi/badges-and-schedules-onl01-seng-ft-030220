d8e676e684add5a3dcf856ae9b7fb90beb156642
Charles Butler <cdbutle@outlook.com>
2020-03-11 19:18:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,7 @@
 # Write your code here.
+# Write a "badge_maker" method 
+   #Method must output "Hello, my name is Arel." when provided name "Arel"
+   
+#write a "batch_badge_creator" method that takes an array of names and returns an array of badge messages
+#write a method called "assign_rooms" takes list of speakers and assigns each to a room 1 for each room
+     #7 rooms  -> 
